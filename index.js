db6500714138bbcf47ff799866db2a800f1d8223
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-06 23:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
     let numberStart = parseInt(startBlock)
     let numberEnd = parseInt(endBlock)
-    let range = Math.abs(numberEnd - numberStart)
+    let range = Math.abs(numberStart - numberEnd)
     if (range < blockRange) {
       return `within range by ${blockRange - range}`
     } else {
