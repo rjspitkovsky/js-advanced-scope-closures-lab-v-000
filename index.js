bae4b323df94f5dfeedeb745556bf7b1b99fefe9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-07 00:02:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
+    let numberStart = parseInt(startBlock)
+    let numberEnd = parseInt(endBlock)
+    let range = Math.abs(numberStart - numberEnd)
+    if (range < blockRange) {
+      return `within range by ${blockRange - range}`
+    } else {
+      return `${range - blockRange} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(percentage) {
+  return function(bill) {
+    return bill * percentage
+  }
+}
+
+function createDriver() {
+  let driverId = 0
+  return class {
+    constructor(name) {
+      this.name = name
+      this.id = ++driverId
+    }
+  }
+}
